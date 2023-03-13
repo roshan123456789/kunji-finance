efffe9fc674b69daf60ba2027f343f6decc6f43c
dbale-kunji <diego@kunji.io>
2023-03-13 20:34:16 -0300

    config: set hardhat test tools

--- contracts/libraries/AddressToAddressMapLib.sol
+++ contracts/libraries/AddressToAddressMapLib.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
 import {EnumerableMapUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
 
 library AddressToAddressMapLib {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,18 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "@nomiclabs/hardhat-ethers"
-
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  paths: {
+    sources: "./contracts",
+    tests: "./tests",
+  },
+  solidity: "0.8.9",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
