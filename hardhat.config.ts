c8f649e9f4f9d7c2fbbf5f9b326ff71847a20be4
maksim@kunji.io <maksim@kunji.io>
2023-04-06 19:07:55 +0300

    update: project settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,8 +15,20 @@ const config: HardhatUserConfig = {
   },
   solidity: "0.8.9",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    hardhat: {
+      forking: {
+        url: process.env.ARBITRUM_NODE || "",
+        blockNumber: 77400001,
+        enabled: true,
+      },
+    },
+    arbitrum: {
+      url: process.env.ARBITRUM_NODE || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    arbitrum_test: {
+      url: process.env.TEST_ARBITRUM_NODE || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
@@ -27,6 +39,9 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 100000000,
   },
 };
 
